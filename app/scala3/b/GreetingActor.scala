530b9ba41322876750e09eba6dab64082acdf9ed
Heiner Bogadtke <Heiner.Bogadtke@epost-dev.de>
2017-02-15 17:15:09 +0100

    added failure handling and implicit deathwatch/lifecycle

--- app/scala3/b/GreetingActor.scala
+++ app/scala3/b/GreetingActor.scala
@@ -21,21 +21,18 @@ class GreetingActor extends Actor with ActorLogging {
   import CapitalizationActor._
   import GreetingActor._
 
-  implicit val executionContext = context.dispatcher
   val upper = context.actorOf(CapitalizationActor.props, "capitalizer")
-  context.watch(upper)
 
   def receive = {
     case HiMyNameIs(name) => upper forward Capitalize(name)
-    case AlsoHiMyNameIs(name) => upper forward CapitalizeAsk(name)
-    case Terminated => log.info(s"watched $upper died")
     case msg => log.info(s"greeting actor got unknown message: $msg")
   }
+
+  override def postStop(): Unit = log.info("GreetingActor was terminated")
 }
 object GreetingActor {
   val props = Props[GreetingActor]
   case class HiMyNameIs(name: String)
-  case class AlsoHiMyNameIs(name: String)
 }
 
 
@@ -45,20 +42,16 @@ class CapitalizationActor extends Actor with ActorLogging {
   val picker = context.actorOf(AddressPickActor.props, name = "addressPicker")
 
   def receive = {
-    // deliver failure to sender to avoid ask timeout so you can see what went wrong where
-//    case Capitalize(str) =>
-
     case Capitalize(str) =>
       picker forward Name(capitalize(str))
     case CapitalizeAsk(str) =>
-      val response = capitalize(str)
-      // functional try to catch an exception and avoid ask timeout
-//      val response = Try(capitalize(str)) match {
-//        case Success(capitalized) =>
-//          capitalized
-//        case Failure(exception) =>
-//          Status.Failure(exception)
-//      }
+      sender ! capitalize(str)
+    case CapitalizeAskFailHandle(str) =>
+      // functional try to catch an exception and avoid ask timeout so the sender can see what went wrong where
+      val response = Try(capitalize(str)) match {
+        case Success(capitalized) => capitalized
+        case Failure(exception) => Status.Failure(exception)
+      }
       sender ! response
   }
 
@@ -67,12 +60,16 @@ class CapitalizationActor extends Actor with ActorLogging {
   private def capitalize(str: String): String = if (str.startsWith("?")) throwUp() else str.capitalize
 
   private def throwUp(): Nothing = throw new RuntimeException("HUÄRGH")
+
+//  override def preStart(): Unit = log.info("CapitalizationActor was started")
+  override def postStop(): Unit = log.info("CapitalizationActor was stopped")
 }
 object CapitalizationActor {
   val props = Props[CapitalizationActor]
 
   case class Capitalize(str: String)
   case class CapitalizeAsk(str: String)
+  case class CapitalizeAskFailHandle(str: String)
 }
 
 
@@ -84,8 +81,10 @@ class AddressPickActor extends Actor with ActorLogging {
       val address = addresses(rnd.nextInt(addresses.length))
       sender ! s"Hi $name, new $address."
   }
-}
 
+//  override def preStart(): Unit = log.info(s"AddressPickActor was started")
+  override def postStop(): Unit = log.info("AddressPickActor was stopped")
+}
 object AddressPickActor {
   case class Name(firstname: String)
   val props = Props[AddressPickActor]
--- app/scala3/b/TestB.scala
+++ app/scala3/b/TestB.scala
@@ -3,55 +3,76 @@ package scala3.b
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorSystem, PoisonPill }
 import akka.pattern._
 import akka.util.Timeout
-import scala3.b.GreetingActor.HiMyNameIs
-import scala3.b.CapitalizationActor.CapitalizeAsk
 import scala3.b.AddressPickActor.Name
-import scala.util.Success
-import scala.util.Failure
-import scala3.b.GreetingActor.AlsoHiMyNameIs
+import scala3.b.CapitalizationActor.{ CapitalizeAsk, CapitalizeAskFailHandle }
+import scala3.b.GreetingActor.HiMyNameIs
 
 object TestB {
 
   def main(args: Array[String]): Unit = {
-    val system = ActorSystem("test-system")
-    implicit val ex = system.dispatcher
-    implicit val timeout: Timeout = 5.seconds
+    val actorSystem = ActorSystem("test-system")
+    implicit val ex = actorSystem.dispatcher
+    implicit val timeout: Timeout = 2.seconds
 
-    /* Forwarding behaves kind of like a public method.
-     * The actors know each other but you don't have to */
-    val greeter = system.actorOf(GreetingActor.props, "gruetzieh")
+    /*
+     * Forwarding behaves kind of like a public method.
+     * The actors know each other but you don't have to
+     */
+    val greeter = actorSystem.actorOf(GreetingActor.props, "gruetzieh")
 
     val greet = greeter ? HiMyNameIs("franz")
     greet.map(println(_))
 
-    // Or you can just ask everything yourself and flatMap it
-    val cap = system.actorOf(CapitalizationActor.props, "captain")
-    val pick = system.actorOf(AddressPickActor.props, "picker")
+    /*
+     *  Or you can just ask everything yourself and flatMap it
+     */
+    val cap = actorSystem.actorOf(CapitalizationActor.props, "captain")
+    val pick = actorSystem.actorOf(AddressPickActor.props, "picker")
 
     val otherGreet = (cap ? CapitalizeAsk("fritz"))
-    .mapTo[String]
-    .flatMap(pick ? Name(_))
-    .mapTo[String]
+      .mapTo[String]
+      .flatMap(pick ? Name(_))
+      .mapTo[String]
 
     otherGreet.foreach(println)
 
-    //What if something breaks?
+    /*
+     *  What if something breaks?
+     *  *hint* timeout because no answer is given
+     */
     val iHopeThisWorks = (cap ? CapitalizeAsk("?franz")).mapTo[String]
     try {
-      val stuff = Await.result(iHopeThisWorks, 2.seconds)
+      val stuff = Await.result(iHopeThisWorks, 3.seconds)
     } catch {
-      case e: Exception => e.printStackTrace()
+      case e: Exception =>
+        println("Non handled ask:")
+        e.printStackTrace()
+//        e.printStackTrace(System.)
+        println()
     }
 
-   val letsSee = (greeter ? AlsoHiMyNameIs("?Carl")).mapTo[String]
-   try {
-      val alsoStuff = Await.result(letsSee, 2.seconds)
+    Thread.sleep(4000)
+
+    /*
+     * Is this more useful?
+     */
+    val letsSee = (cap ? CapitalizeAskFailHandle("?Carl")).mapTo[String]
+    try {
+      val alsoStuff = Await.result(letsSee, 3.seconds)
     } catch {
-      case e: Exception => e.printStackTrace()
+      case e: Exception =>
+        println("Handled ask:")
+        e.printStackTrace()
+        println()
     }
+
+    Thread.sleep(4000)
+
+    // cleanup of children?
+    greeter ! PoisonPill
 
     while (true) { Thread.sleep(1000) }
   }
